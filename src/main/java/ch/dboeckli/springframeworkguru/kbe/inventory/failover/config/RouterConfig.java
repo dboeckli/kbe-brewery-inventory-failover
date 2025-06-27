b812526b1661eabad103adca782841108e844caa
dboeckli <dboeckli@gmail.com>
2025-06-27 07:34:13 +0200

    add helm deployment.yaml

--- src/main/java/ch/dboeckli/springframeworkguru/kbe/inventory/failover/config/RouterConfig.java
+++ src/main/java/ch/dboeckli/springframeworkguru/kbe/inventory/failover/config/RouterConfig.java
@@ -4,21 +4,18 @@ import ch.dboeckli.springframeworkguru.kbe.inventory.failover.web.InventoryHandl
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
-/**
- * Created by jt on 3/13/20.
- */
 @Configuration
 public class RouterConfig {
 
     @Bean
-    public RouterFunction orderRouter(InventoryHandler handler) {
-
+    public RouterFunction<ServerResponse> orderRouter(InventoryHandler handler) {
         return route(GET("/inventory-failover").and(accept(APPLICATION_JSON)), handler::listInventory);
     }
 }
--- src/main/java/ch/dboeckli/springframeworkguru/kbe/inventory/failover/web/InventoryHandler.java
+++ src/main/java/ch/dboeckli/springframeworkguru/kbe/inventory/failover/web/InventoryHandler.java
@@ -1,6 +1,7 @@
 package ch.dboeckli.springframeworkguru.kbe.inventory.failover.web;
 
 import ch.guru.springframework.kbe.lib.dto.BeerInventoryDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
@@ -8,25 +9,25 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
 import java.time.OffsetDateTime;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
-/**
- * Created by jt on 3/9/20.
- */
 @Component
+@Slf4j
 public class InventoryHandler {
+
     public Mono<ServerResponse> listInventory(ServerRequest request){
+        log.debug("invoking handler listInventory: {} ", request);
         return ServerResponse.ok()
-                .contentType(MediaType.APPLICATION_STREAM_JSON)
-                .body(Mono.just(Arrays.asList(
-                        BeerInventoryDto.builder()
-                                .id(UUID.randomUUID())
-                                .beerId(UUID.fromString("00000000-0000-0000-0000-000000000000"))
-                                .quantityOnHand(999)
-                                .createdDate(OffsetDateTime.now())
-                                .lastModifiedDate(OffsetDateTime.now())
-                                .build())), List.class);
+                .contentType(MediaType.APPLICATION_NDJSON)
+                .body(Mono.just(Collections.singletonList(
+                    BeerInventoryDto.builder()
+                        .id(UUID.randomUUID())
+                        .beerId(UUID.fromString("00000000-0000-0000-0000-000000000000"))
+                        .quantityOnHand(999)
+                        .createdDate(OffsetDateTime.now())
+                        .lastModifiedDate(OffsetDateTime.now())
+                        .build())), List.class);
     }
 }
