b083dfee528aed7ad1301126659068abec955cd2
Dominique Boeckli <dboeckli@gmail.com>
2026-04-08 07:10:55 +0200

    Feature/upgrade jdk and maven (#60)
    * format
    
    * upgrad jdk
    
    * remove editorconfig
    
    * spotless

--- src/main/java/ch/dboeckli/springframeworkguru/kbe/inventory/failover/web/InventoryHandler.java
+++ src/main/java/ch/dboeckli/springframeworkguru/kbe/inventory/failover/web/InventoryHandler.java
@@ -17,17 +17,17 @@ import java.util.UUID;
 @Slf4j
 public class InventoryHandler {
 
-    public Mono<ServerResponse> listInventory(ServerRequest request){
+    public Mono<ServerResponse> listInventory(ServerRequest request) {
         log.info("invoking handler listInventory: {} ", request);
         return ServerResponse.ok()
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(Mono.just(Collections.singletonList(
-                    BeerInventoryDto.builder()
-                        .id(UUID.randomUUID())
-                        .beerId(UUID.fromString("00000000-0000-0000-0000-000000000000"))
-                        .quantityOnHand(999)
-                        .createdDate(OffsetDateTime.now())
-                        .lastModifiedDate(OffsetDateTime.now())
-                        .build())), List.class);
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(Mono.just(Collections.singletonList(BeerInventoryDto.builder()
+                .id(UUID.randomUUID())
+                .beerId(UUID.fromString("00000000-0000-0000-0000-000000000000"))
+                .quantityOnHand(999)
+                .createdDate(OffsetDateTime.now())
+                .lastModifiedDate(OffsetDateTime.now())
+                .build())), List.class);
     }
+
 }
